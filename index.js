38a2dd9bbe77d1fb5b9e923627c19fa7bc144a24
StudentMaster <student@kodaktor.ru>
2018-04-11 04:09:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function transformFahrToCels(degrees) {
   return (5 / 9) * (degrees - 32);
 }
+// функция принимает на вход число градусов Фаренгейта и возвращает число градусов 
 
 const f1 = 451;
 console.log(transformFahrToCels(f1)); // 232.777
